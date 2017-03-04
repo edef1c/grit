d5a6b04afc449661034d243910584e4c1d01d09f
edef <edef@edef.eu>
2017-03-04 01:23:03 +0100

    add gulp::split_fuzz for fuzzing chunk boundaries

--- gulp/src/lib.rs
+++ gulp/src/lib.rs
@@ -174,3 +174,20 @@ impl Parse for Leb128 {
     Result::Incomplete(self)
   }
 }
+
+pub fn split_fuzz<'a, P: Parse + Default>(data: &'a [u8]) where ParseResult<'a, P>: Debug + Eq {
+  if data.len() < 4 { return }
+  let n = ((data[0] as usize) <<  0)
+        | ((data[1] as usize) <<  8)
+        | ((data[2] as usize) << 16)
+        | ((data[3] as usize) << 24);
+  let data = &data[4..];
+  if data.len() < n { return }
+  let immediate = P::default().parse(data);
+  let incremental = match P::default().parse(&data[..n]) {
+    Result::Err(e) => Result::Err(e),
+    Result::Incomplete(p) => p.parse(&data[n..]),
+    Result::Ok(v, tail) => Result::Ok(v, &data[n - tail.len()..])
+  };
+  assert_eq!(immediate, incremental);
+}
