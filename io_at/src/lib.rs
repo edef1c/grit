fb6faf57b02d972740c93aaa35eb436b09a85b98
edef <edef@edef.eu>
2017-03-04 02:48:53 +0100

    implement io_at::{ReadAt, WriteAt} for references

--- io_at/src/lib.rs
+++ io_at/src/lib.rs
@@ -13,7 +13,21 @@ pub trait ReadAt {
   fn read_at(&self, off: u64, buf: &mut [u8]) -> Result<usize, Self::Err>;
 }
 
+impl<'a, R: ReadAt> ReadAt for &'a R {
+  type Err = R::Err;
+  fn read_at(&self, off: u64, buf: &mut [u8]) -> Result<usize, Self::Err> {
+    R::read_at(self, off, buf)
+  }
+}
+
 pub trait WriteAt {
   type Err: Debug;
   fn write_at(&self, off: u64, buf: &[u8]) -> Result<usize, Self::Err>;
+}
+
+impl<'a, W: WriteAt> WriteAt for &'a W {
+  type Err = W::Err;
+  fn write_at(&self, off: u64, buf: &[u8]) -> Result<usize, Self::Err> {
+    W::write_at(self, off, buf)
+  }
 }
